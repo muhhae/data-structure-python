8364dac97b7322dd0ed1e0f9fb8bb5e1354a979f
Muhhae <haekal.muhamat@gmail.com>
2023-09-06 15:24:43 +0700

    Something

--- FileSearcher.py
+++ FileSearcher.py
@@ -1,13 +1,14 @@
 import os
 
-def ListFile(path):
-    p = os.listdir(path)
-    for e in p:
+def ListFile(path, space = 0):
+    listDir = os.listdir(path)
+    for e in listDir:
         dir = os.path.join(path, e)
+        for i in range(space):
+            print("\t", sep="", end="")
         if os.path.isdir(dir):
-            print(f"folder : {dir}")
-            ListFile(dir)
+            print(f"folder:{e}")
+            ListFile(dir, space + 1)
         else:
-            print(f"file : {dir}")
-
+            print(f"file:{e}")
 ListFile("D:/CPP_Fun_Project")
