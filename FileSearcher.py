13c91876f2d06475be1391ad5dee52d8abf9a098
Muhhae <haekal.muhamat@gmail.com>
2023-09-10 12:09:58 +0700

    Refactor

--- FileSearcher.py
+++ FileSearcher.py
@@ -1,14 +1,20 @@
 import os
 
-def ListFile(path, space = 0):
-    listDir = os.listdir(path)
-    for e in listDir:
-        dir = os.path.join(path, e)
-        for i in range(space):
-            print("\t", sep="", end="")
-        if os.path.isdir(dir):
-            print(f"folder:{e}")
-            ListFile(dir, space + 1)
-        else:
-            print(f"file:{e}")
-ListFile("D:/CPP_Fun_Project")
+def ListFile(path, space = 0, fullPath = False, printType = False):
+    for i in range(space):
+        print("\t", sep="", end="")
+    print("|-", sep="", end="")
+    printed = path if fullPath else os.path.basename(path)
+    filetype = ""
+    if os.path.isdir(path):
+        if printType : filetype = "folder:"
+        print(f"{filetype}{printed}")
+        listDir = os.listdir(path)
+        for e in listDir:
+            ListFile(os.path.join(path, e), space + 1, fullPath, printType)
+    else:
+        if printType: filetype = "file:"
+        print(f"{filetype}{printed}")
+
+
+ListFile("D:/CPP_Fun_Project", 0, False, True)
---
+++ HanoiTower.py
@@ -0,0 +1,31 @@
+import pygame
+
+# pygame setup
+pygame.init()
+
+displayList = pygame.display.get_desktop_sizes()
+
+screen = pygame.display.set_mode(displayList[0])
+clock = pygame.time.Clock()
+running = True
+dt = 0
+
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_ESCAPE]:
+        running = False
+
+    pygame.display.flip()
+
+    dt = clock.tick(60) / 1000
+
+pygame.quit()
