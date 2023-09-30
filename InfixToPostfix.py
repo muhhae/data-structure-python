6fcce5b56fcb4cdaa1bca61be1fb0d45814c31cf
Muhhae <haekal.muhamat@gmail.com>
2023-09-30 17:09:32 +0700

    update infixtotpostfix

--- InfixToPostfix.py
+++ InfixToPostfix.py
@@ -1,18 +1,23 @@
 from rich import print
 from rich.table import Table
+
+
 def isOp(c):
     op = ["+", "/", "*", "-", "^", "(", ")"]
     return c in op
+
 
 def opVal(op: str):
     match op:
         case "+" | "-": return 0
         case "*" | "/": return 1
-        case "^": return 3
+        case "^": return 2
+
 
 def listprint(l: list):
     for e in l:
         print(e, end="")
+
 
 def listToStr(l: list):
     s = ""
@@ -20,7 +25,9 @@ def listToStr(l: list):
         s += str(e)
     return s
 
+
 def infixToPostfix(n: str):
+    print("infix:", n)
     result = []
     aux = []
 
@@ -32,14 +39,26 @@ def infixToPostfix(n: str):
                   title_justify="center",
                   caption_justify="center",)
     step = 0
-    for i,c in enumerate(n):
-        if c == " ": continue
+    for i, c in enumerate(n):
+        if c == " ":
+            continue
+        if c == "-" and (isOp(n[i-1]) or i == 0):
+            cTemp += c
+            if i == len(n) - 1:
+                result.append(cTemp)
+                result.append(" ")
+                step += 1
+                table.add_row(str(step), cTemp, listToStr(
+                    aux), listToStr(result))
+                table.add_section()
+            continue
         if isOp(c):
             if cTemp != "":
                 result.append(cTemp)
                 result.append(" ")
                 step += 1
-                table.add_row(str(step), cTemp, listToStr(aux), listToStr(result))
+                table.add_row(str(step), cTemp, listToStr(
+                    aux), listToStr(result))
                 table.add_section()
                 cTemp = ""
         if not isOp(c):
@@ -48,7 +67,8 @@ def infixToPostfix(n: str):
                 result.append(cTemp)
                 result.append(" ")
                 step += 1
-                table.add_row(str(step), cTemp, listToStr(aux), listToStr(result))
+                table.add_row(str(step), cTemp, listToStr(
+                    aux), listToStr(result))
                 table.add_section()
             continue
         elif c == '(':
@@ -59,7 +79,7 @@ def infixToPostfix(n: str):
                 result.append(" ")
             aux.pop()
         else:
-            while len(aux) > 0 and aux[-1] != "(" and opVal(aux[-1]) > opVal(c):
+            while len(aux) > 0 and aux[-1] != "(" and opVal(aux[-1]) >= opVal(c):
                 result.append(aux.pop())
                 result.append(" ")
             aux.append(c)
@@ -80,14 +100,15 @@ def infixToPostfix(n: str):
     print("postfix\t:", strResult)
     return result
 
+
 def calculatePostfix(n: list):
-    result=[]
+    result = []
     print("\nCalculating...\n")
     for c in n:
         if c == " ":
             continue
         if not isOp(c):
-            result.append(int(c))
+            result.append(float(c))
             continue
 
         b = result.pop()
@@ -95,7 +116,7 @@ def calculatePostfix(n: list):
 
         match c:
             case "*":
-                print(a, "*" ,b, "=", a*b)
+                print(a, "*", b, "=", a*b)
                 result.append(a*b)
             case "+":
                 print(a, "+", b, "=", a+b)
@@ -114,6 +135,7 @@ def calculatePostfix(n: list):
     print("result\t:", res)
     return res
 
+
 def initTugas(s: str):
     print("infix\t:", s)
     Str = infixToPostfix(s)
@@ -123,14 +145,16 @@ def initTugas(s: str):
             break
     print()
 
-Str = [
-    "2*3^4*5+9",
-    "A*B+C*D",
-    "2 * ( 3 + 5 )",
-    "2*(3^(1+1) + 5 * ((1+1)*(1+1)))",
-    "23 + 34 * 21"
-]
 
-print("-- Infix To Postfix -- \n")
-for e in Str:
-    initTugas(e)
+
+if __name__ == "__main__":
+    Str = [
+        "2*3^4*5+9",
+        "A*B+C*D",
+        "2 * ( 3 + 5 )",
+        "2*(3^(1+1) + 5 * ((1+1)*(1+1)))",
+        "23 + 34 * 21"
+    ]
+    print("-- Infix To Postfix -- \n")
+    for e in Str:
+        initTugas(e)
